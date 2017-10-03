93482089703b4d507da5d2d6b764a6bac4d09455
KevinPham <kevinpham15@gmail.com>
2017-10-02 22:39:29 -0700

    complete

---
+++ comparelist.py
@@ -0,0 +1,9 @@
+list_one = [1,2,5,6,2]
+# list_two = [1,2,5,6,2]
+list_two = [1,2,5,6,5,3]
+
+
+if set(list_one) == set(list_two):
+    print "The lists are identical"
+else:
+    print "Lists are not the same"
